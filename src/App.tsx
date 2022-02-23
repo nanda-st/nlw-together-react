01dde25272793416c05ce63d0c168ed7ef78b6e7
Nanda <nana.script.js@gmail.com>
2022-02-23 15:11:52 -0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <>
-      <NewRoom/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/rooms/new" element={<NewRoom />}/>
+      </Routes>
+    </Router>
   );
 }
 
